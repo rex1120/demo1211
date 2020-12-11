e7b4860cfc3ac0d2d2ca9257fafa8d1d962a6e06
骆超 <895532817@qq.com>
2020-12-11 17:42:08 +0800

    Initial commit

---
+++ src/main/java/com/example/demo1211/Demo1211Application.java
@@ -0,0 +1,13 @@
+package com.example.demo1211;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1211Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo1211/Demo1211ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo1211;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
